a6d07d7925c0e2f63493152a21545c37f1c61b6d
Angel Resendiz <angel.resendiz@housecallpro.com>
2023-02-23 16:28:15 -0600

    senbird sdk v4 issue

---
+++ README.md
@@ -0,0 +1,20 @@
+## Instructions.
+First, create an `.env.local` file and set the required variables (you can use `.env.local` as a start)
+
+Then, install dependencies:
+```bash
+npm install
+# or
+yarn
+```
+Then, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000/api/unread_messages_v3](http://localhost:3000/api/unread_messages_v3) to see the sendbird javascript sdk v3 working.
+
+Open [http://localhost:3000/api/unread_messages_v3](http://localhost:3000/api/unread_messages_v3) to reproduce the issue when using sendbird javascript sdk v4.
---
+++ pages/api/unread_messages_v4.js
@@ -0,0 +1,40 @@
+import initMiddleware from "@/utils/initMiddleware";
+import SendbirdChat from "@sendbird/chat";
+import { GroupChannelModule } from "@sendbird/chat/groupChannel";
+import Cors from "cors";
+
+const cors = initMiddleware(Cors({ origin: "*", methods: ["GET"] }));
+
+const serverError = ({ res, error: { code, name, message } }) => {
+  res.status(500).json({ code, name, message });
+};
+
+const badRequestError = {
+  code: 400,
+  name: "Bad Request",
+  message: "Username and Token are required",
+};
+
+export default async function handler(req, res) {
+  await cors(req, res);
+
+  const appId = process.env.SB_APP_ID || "";
+  const user = process.env.SB_USER_ID || "";
+  const token = process.env.SB_TOKEN || "";
+  if (!appId || !token || !user) return res.status(400).json(badRequestError);
+
+  const sb = await SendbirdChat.init({
+    appId,
+    modules: [new GroupChannelModule()],
+  });
+
+  try {
+    await sb.connect(user, token);
+    const unreadMessages = await sb.groupChannel.getTotalUnreadMessageCount();
+    await sb.disconnect();
+    res.status(200).json({ unread_messages: unreadMessages });
+  } catch (error) {
+    await sb.disconnect();
+    serverError({ res, error });
+  }
+}
